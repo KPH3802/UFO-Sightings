bd3c9eb36ec16fcabdc4a0aaf7eb18959801477b
KPH3802 <kph3802@gmail.com>
2019-07-25 13:37:37 -0500

    Trying to search for more than one term

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-// from data.js
+// from data.js 
 var tableData = data;
 
 // Use D3 to create the table body
 var tbody = d3.select("tbody");
+
+// Populate the table with all the data first 
 
 data.forEach((UFOSighting) => {
    var row = tbody.append("tr");
@@ -33,30 +35,21 @@ button.on("click", function() {
    console.log('Date Input:', dateInput);
 
   // Filter by date input
-  // filter 'iterates' over a list of items, and performs a check on each item
-  // example: greater_than_five_list = array.filter(function(value){ return value >= 5 })
-  // var filtered_dates2 = data.filter(function(obj){ return obj['datetime'] === dateInput });
 
   var filtered_dates = data.filter(
     function(obj){ 
       // return obj['datetime'] === dateInput && obj['state'] == 'ca' // all in one
       return obj['datetime'] === dateInput
     }
-  );
+  )
 
-  // Filter by state input
-  // filtered_states = filtered_dates.filter(
-  //   function(obj){ 
-  //     return obj['state'] == stateInput;
-  //   }
-  // )
-  
 
   console.log('Filtered Dates:', filtered_dates);
 
   // remove the data from tbody
   tbody.html('');
 
+// Put the data filtered on dates into the table
   filtered_dates.forEach((UFOSighting) => {
     var row = tbody.append("tr");
     Object.entries(UFOSighting).forEach(([key, value]) => {
@@ -64,5 +57,34 @@ button.on("click", function() {
       cell.text(value);
     });
   });
+});
 
-})
+//   button.on("click", function() {
+//     // Prevents the page from refreshing
+//     d3.event.preventDefault();
+
+//        // Select the input element (state)
+//    var inputElement2 = d3.select("#state");
+//    console.log('Input Element:', inputElement2);
+  
+//    // Get value
+//    var stateInput = inputElement2.property('value');
+//    console.log('State Input:', stateInput);
+
+//   // Filter by state input
+//   var filtered_states = filtered_dates.filter(
+//     function(obj){ 
+//       return obj['state'] === stateInput;
+//     }
+//   );
+    
+//   // tbody.html('');
+
+//   filtered_states.forEach((UFOSighting) => {
+//     var row = tbody.append("tr");
+//     Object.entries(UFOSighting).forEach(([key, value]) => {
+//       var cell = row.append("td");
+//       cell.text(value);
+//     });
+//   });
+// });

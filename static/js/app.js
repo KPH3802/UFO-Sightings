9b3dedacb943083be4bdda7dd4766ab0dd1e84d6
KPH3802 <kph3802@gmail.com>
2019-07-26 08:55:23 -0500

    Still working Lowercase but filters all work

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 // from data.js 
-var tableData = data;
+var filterUFOdata = data;
 
 // Use D3 to create the table body
 var tbody = d3.select("tbody");
@@ -7,11 +7,11 @@ var tbody = d3.select("tbody");
 // Populate the table with all the data first 
 
 data.forEach((UFOSighting) => {
-  var row = tbody.append("tr");
-  Object.entries(UFOSighting).forEach(([key, value]) => {
-    var cell = row.append("td");
-    cell.text(value);
-  });
+    var row = tbody.append("tr");
+    Object.entries(UFOSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
 });
 
 
@@ -21,75 +21,118 @@ var button = d3.select("#filter-btn");
 console.log('Here is the button:', button);
 
 button.on("click", function () {
-  // Prevents the page from refreshing
-  d3.event.preventDefault();
+    // Prevents the page from refreshing
+    d3.event.preventDefault();
 
-  console.log('HEY THE BUTTON CLICK HANDLER WORKS!');
+    console.log('HEY THE BUTTON CLICK HANDLER WORKS!');
 
-  // Select the input element (date time)
-  var inputElement = d3.select("#datetime");
-  console.log('Input Element:', inputElement);
+    // Select the input element (date time)
+    var inputElement1 = d3.select("#datetime");
+    console.log('Input Element1:', inputElement1);
 
-  // Get value
-  var dateInput = inputElement.property('value');
-  console.log('Date Input:', dateInput);
+    // Get value
+    var dateInput = inputElement1.property('value');
+    console.log('Date Input:', dateInput);
 
-  // Select the input element (state)
-  var inputElement2 = d3.select("#state");
-  console.log('Input Element:', inputElement2);
+    // Select the input element (city)
+    var inputElement2 = d3.select("#city");
+    console.log('Input Element2:', inputElement2);
 
-  // Get value
-  var stateInput = inputElement2.property('value');
-  console.log('State Input:', stateInput);
+    // Get value
+    var cityInput = inputElement2.property('value');
+    console.log('City Input:', cityInput);
 
-  // Select the input element (state)
-  var inputElement2 = d3.select("#state");
-  console.log('Input Element:', inputElement2);
+    // Select the input element (state)
+    var inputElement3 = d3.select("#state");
+    console.log('Input Element3:', inputElement3);
 
-  // Get value
-  var stateInput = inputElement2.property('value');
-  console.log('State Input:', stateInput);
+    // Convert to lowercase to handle both upper and lower case entries
+    // var inputElement3 = inputElement3.toLowerCase();
+    
 
+    // Get value
+    var stateInput = inputElement3.property('value');
+    console.log('State Input:', stateInput);
 
-  // Filter by date input
-  if (stateInput !== "") {
+    // var stateInput2 = stateInpu
+    // console.log('Type of State varilable:', typeof stateInput2);
 
-    var filtered_dates = data.filter(
-      function (obj) {
-        // return obj['datetime'] === dateInput && obj['state'] == 'ca' // all in one
-        return obj['datetime'] === dateInput
-      }
-    );
-    // Filter by state input
-    var filtered_states = filtered_dates.filter(
-      function (obj) {
-        return obj['state'] === stateInput;
-      }
-    );
+    // Select the input element (country)
+    var inputElement4 = d3.select("#country");
+    console.log('Input Element:', inputElement4);
 
-  } else {
-    var filtered_states = data.filter(
-      function (obj) {
-        // return obj['datetime'] === dateInput && obj['state'] == 'ca' // all in one
-        return obj['datetime'] === dateInput
-      }
-    );
+    // Get value
+    var countryInput = inputElement4.property('value');
+    console.log('Country Input:', countryInput);
+
+    // Select the input element (shape)
+    var inputElement5 = d3.select("#shape");
+    console.log('Input Element:', inputElement5);
+
+    // Get value
+    var shapeInput = inputElement5.property('value');
+    console.log('Shape Input:', shapeInput);
 
 
-  };
-  console.log('Filtered Dates:', filtered_dates);
+    // Filter by date input
+    if (dateInput !== "") {
 
-  // remove the data from tbody
-  tbody.html('');
+        filterUFOdata = filterUFOdata.filter(
+            function (obj) {
+                // return obj['datetime'] === dateInput && obj['state'] == 'ca' // all in one
+                return obj['datetime'] === dateInput
+            }
+        );
+    }
+    if (cityInput !== "") {
 
-  // Put the data filtered on dates into the table
-  filtered_states.forEach((UFOSighting) => {
-    var row = tbody.append("tr");
-    Object.entries(UFOSighting).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+        filterUFOdata = filterUFOdata.filter(
+            function (obj) {
+                // return obj['datetime'] === dateInput && obj['state'] == 'ca' // all in one
+                return obj['city'] === cityInput
+            }
+        );
+    }
+    if (stateInput !== "") {
+
+        filterUFOdata = filterUFOdata.filter(
+            function (obj) {
+                // return obj['datetime'] === dateInput && obj['state'] == 'ca' // all in one
+                return obj['state'] === stateInput
+            }
+        );
+    }
+    if (countryInput !== "") {
+
+        filterUFOdata = filterUFOdata.filter(
+            function (obj) {
+                // return obj['datetime'] === dateInput && obj['state'] == 'ca' // all in one
+                return obj['country'] === countryInput
+            }
+        );
+    }
+    if (shapeInput !== "") {
+
+        filterUFOdata = filterUFOdata.filter(
+            function (obj) {
+                // return obj['datetime'] === dateInput && obj['state'] == 'ca' // all in one
+                return obj['shape'] === shapeInput
+            }
+        );
+    }
+
+
+    // remove the data from tbody
+    tbody.html('');
+
+    // Put the data filtered on dates into the table
+    filterUFOdata.forEach((UFOSighting) => {
+        var row = tbody.append("tr");
+        Object.entries(UFOSighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-  });
 });
 
 
